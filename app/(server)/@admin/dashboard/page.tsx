e37b44075e587affeca8aa3566b6615e2a4a9e85
muziyuan <gaomuziyuan@icloud.com>
2024-03-25 00:23:39 -0700

    change post to patch

--- app/(server)/@admin/dashboard/page.tsx
+++ app/(server)/@admin/dashboard/page.tsx
@@ -26,7 +26,7 @@ export default function UserManagement() {
 
   const toggleUserStatus = async (userId: string, status: boolean) => {
     try {
-      await axios.post(`${process.env.NEXT_PUBLIC_API_HOST}/users/${userId}`, {
+      await axios.patch(`${process.env.NEXT_PUBLIC_API_HOST}/users/${userId}`, {
         isActive: status,
       });
       setUsers(
